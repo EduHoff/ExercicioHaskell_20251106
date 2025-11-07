991b13ad63e0e51db63aefac7423295301a75abd
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-11-07 12:53:37 -0300

    função para verificar se um elemento repitiu

--- main.hs
+++ main.hs
@@ -7,13 +7,22 @@ removerElementos (h:t)
     | h == head t = removerElementos t
     | otherwise = h : removerElementos t
 
+vER :: (Eq a) => a -> [a] -> Int -> Bool --verificarElementosRepitidos
+vER _ [] acc = acc > 1
+vER x (h:t) acc =
+    if x == h
+        then vER x t (acc+1)
+        else vER x t acc
+
+
 main :: IO()
 main = do
     clear
-    let array = ["a","a","b","c"]
+    let array = ["a","a","b","c","d","b"]
 
     putStrLn ("Original: " ++ show array)
     putStrLn ("Após função: " ++ show (removerElementos array))
+    putStrLn ("Teste função verificação: " ++ show (vER "b" array 0))
 
 
 clear :: IO ()
